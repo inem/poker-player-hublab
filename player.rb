e6fd7fe8a8d4fb6fb1dbdb1bbfb513fa7175a238
Nicola Hughes <nicolarhughes@gmail.com>
2016-04-26 08:53:33 +0100

    Commenting

--- player.rb
+++ player.rb
@@ -4,12 +4,12 @@ class Player
   VERSION = "InFlabWorksHub"
 
   def bet_request(game_state)
-    our_player = nil
-    game_state[:players].each do |player|
-      if player[:name] == "HubLab"
-        our_player = player
-      end
-    end
+    # our_player = nil
+    # game_state[:players].each do |player|
+    #   if player[:name] == "HubLab"
+    #     our_player = player
+    #   end
+    # end
 
     20000
   end

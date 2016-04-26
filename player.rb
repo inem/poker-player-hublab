c74d33de82ff3ddc49bb215eadcb784c9985a02a
Kevin Tse <kevin.tse@prezi.com>
2016-04-26 12:08:10 +0200

    Refactoring to use PocketOdds

--- player.rb
+++ player.rb
@@ -1,4 +1,5 @@
 require_relative "game_state"
+require_relative "pocket_odds"
 
 
 class Player
@@ -6,35 +7,17 @@ class Player
 
   def bet_request(game_state)
     begin
-      odds = PocketOdds.new(GameState.new(game_state))
+      game_state = GameState.new(game_state)
+      pocket_odds = PocketOdds.new(game_state)
+      odds = pocket_odds.percent_pre_flop
       rand(1700)+700 * (1 + odds)
     rescue
-      if pair_or_more?(cards)
-        puts "500"
-        rand(1700)+700
-      else
-        puts "0"
-        rand(75)+75
-      end
+      puts "exception"
+      rand(1700)+700
     end
   end
 
   def showdown(game_state)
 
-  end
-
-  def pair_or_more?(cards)
-    cards.uniq.size < cards.size
-  end
-
-  def hole_cards(game_state)
-    player = our_player(game_state)
-    Array(player["hole_cards"].map{|card| card["rank"]})
-  end
-
-  def our_player(game_state)
-    game_state["players"].each do |player|
-      return player if player["name"] == "HubLab"
-    end
   end
 end
--- player_spec.rb
+++ player_spec.rb
@@ -62,58 +62,9 @@ RSpec.describe Player do
 
   }
 
-  it "detects pairs correctly" do
-    expect(player.hole_cards(sample_game_state)).to eql(["6", "6"])
-
-
-    cards = player.hole_cards(sample_game_state)
-    expect(player.pair_or_more?(cards)).to be true
-
-    cards = player.hole_cards(sample_game_state2)
-    expect(player.pair_or_more?(cards)).to be false
-  end
-
-
-  it "detects pairs correctly" do
-    player = Player.new
-    expect(player.bet_request(sample_game_state)).not_to eql(0)
-  end
-
-
   it "should use pocket calculator to place bets" do
     game_state = GameState.new(sample_game_state)
-    pocket_odds_calculator = PocketOdds.new(game_state)
-    expect(player.bet_request(sample_game_state)).to equal 435
+    expect(player.bet_request(sample_game_state)).to be_within(0.1).of(2600)
 
   end
-
-  # it "detects pairs correctly" do
-  #   player = Player.new
-
-  #   hole_cards = [
-  #               {
-  #                   rank: "6",
-  #                   suit: "hearts"
-  #               },
-  #               {
-  #                   rank: "6",
-  #                   suit: "spades"
-  #               }
-  #           ]
-
-  #   sample_game_state = {
-  #     "players" => [
-  #         {
-  #             "id" => 1,
-  #             "name" => "Alice"
-  #         },
-  #         {
-  #             "id" => 2,
-  #             "name" => "Bob"
-  #         }
-  #     ]
-  #   }
-
-  #   expect(player(sample_game_state).pair_or_more?(hole_cards)).to be_true
-  # end
 end

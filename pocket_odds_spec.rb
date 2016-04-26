fcfe02c242ae720ff1f0a453b701aeecb83448e4
Will Roden <willabides@github.com>
2016-04-26 11:16:46 +0200

    basic pocketodds

--- pocket_odds_spec.rb
+++ pocket_odds_spec.rb
@@ -1,27 +1,26 @@
 require_relative "pocket_odds"
 
 RSpec.describe PocketOdds do
+  subject { described_class.new(game_state) }
+
+  let(:game_state) { double(:game_state, my_cards: cards ) }
+  let(:cards) { [{"rank" => "A", "suit" => "hearts"}, {"rank" => "A", "suit" => "spades"}] }
+
   it "returns 0.85 for our pair vs. 2 undercards" do
-    pocket_odds_calculator = PocketOdds.new
+    expect(subject.percent_pre_flop).to eq 0.85
+  end
 
-    pair = [{"rank" => "6", "suit" => "hearts"}, {"rank" => "6", "suit" => "spades"}]
-    two_undercards = [{"rank" => "5", "suit" => "hearts"}, {"rank" => "4", "suit" => "spades"}]
+  context 'pair of kings' do
+    let(:cards) { [{"rank" => "K", "suit" => "hearts"}, {"rank" => "K", "suit" => "spades"}] }
+    it 'should give a value for a pair of kings' do
+      expect(subject.percent_pre_flop).to eq 0.82
+    end
+  end
 
-    sample_game_state = {
-        "players" => [
-            {
-                "id" => 1,
-                "name" => "HubLab",
-                "hole_cards" => pair
-            },
-            {
-                "id" => 2,
-                "name" => "Bob",
-                "hole_cards" => two_undercards
-            }
-        ]
-    }
-
-    expect(pocket_odds_calculator.percent_pre_flop(sample_game_state)).to eq 0.85
+  context 'shitty cards' do
+    let(:cards) { [{"rank" => "2", "suit" => "hearts"}, {"rank" => "7", "suit" => "spades"}] }
+    it 'should give a value for a pair of kings' do
+      expect(subject.percent_pre_flop).to eq 0
+    end
   end
 end

bdf40c03aaca083900242ccce1226b963b5ea5a9
Ivan Nemytchenko <nemytchenko@gmail.com>
2016-04-26 11:02:49 +0200

    refactoring

--- player_spec.rb
+++ player_spec.rb
@@ -1,39 +1,39 @@
 require_relative "player"
 
 RSpec.describe Player do
+  let(:player) { Player.new }
+  let(:sample_game_state){
+    {
+          "players" => [
+              {
+                  "id" => 1,
+                  "name" => "HubLab",
+                  "hole_cards" => [
 
+                    {
+                        "rank" => "6",
+                        "suit" => "hearts"
+                    },
+                    {
+                        "rank" => "6",
+                        "suit" => "spades"
+                    }
+                ]
+
+              },
+              {
+                  "id" => 2,
+                  "name" => "Bob"
+              }
+          ]
+        }
+
+  }
   it "parses game_state successfully" do
-    player = Player.new
-    sample_game_state = {
-      "players" => [
-          {
-              "id" => 1,
-              "name" => "HubLab",
-              "hole_cards" => [
-
-                {
-                    "rank" => "6",
-                    "suit" => "hearts"
-                },
-                {
-                    "rank" => "6",
-                    "suit" => "spades"
-                }
-            ]
-
-          },
-          {
-              "id" => 2,
-              "name" => "Bob"
-          }
-      ]
-    }
-
     expect(player.hole_cards(sample_game_state)).to eql(["6", "6"])
 
     cards = player.hole_cards(sample_game_state)
     expect(player.pair_or_more?(cards)).to be true
-
   end
 
   # it "detects pairs correctly" do

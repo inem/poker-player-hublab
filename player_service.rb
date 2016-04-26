c7a2a08e99ac0b0e8ae3a575579853697970726d
Gabriel Mansour <gabrielmansour@gmail.com>
2016-04-26 14:45:15 +0200

    string keys

--- player_service.rb
+++ player_service.rb
@@ -7,9 +7,9 @@ set :bind, '0.0.0.0'
 
 post "/" do
   if params[:action] == 'bet_request'
-    Player.new.bet_request(JSON.parse(params[:game_state], symbolize_keys: true)).to_s
+    Player.new.bet_request(JSON.parse(params[:game_state])).to_s
   elsif params[:action] == 'showdown'
-    Player.new.showdown(JSON.parse(params[:game_state], symbolize_keys: true))
+    Player.new.showdown(JSON.parse(params[:game_state]))
     'OK'
   elsif params[:action] == 'version'
     Player::VERSION

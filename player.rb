239dfc698f77f058bc7d5a0cb93a196f05e9af25
Ivan Nemytchenko <nemytchenko@gmail.com>
2016-04-26 10:04:56 +0200

    new logic

--- player.rb
+++ player.rb
@@ -4,17 +4,32 @@ class Player
   VERSION = "InFlabWorksHub"
 
   def bet_request(game_state)
-    # our_player = nil
-    # game_state[:players].each do |player|
-    #   if player[:name] == "HubLab"
-    #     our_player = player
-    #   end
-    # end
+    player = get_player(game_state)
 
-    20000
+    if pair_or_more?(player)
+      20000
+    else
+      500
+    end
   end
 
   def showdown(game_state)
 
   end
+
+  def pair_or_more?(cards)
+    cards.size.uniq < cards.size
+  end
+
+  def hole_cards(player)
+    player["hole_cards"].map{|card| card["rank"]}
+  end
+
+  def player(game_state)
+    game_state[:players].each do |player|
+      return player if player[:name] == "HubLab"
+    end
+  end
 end
+
+
